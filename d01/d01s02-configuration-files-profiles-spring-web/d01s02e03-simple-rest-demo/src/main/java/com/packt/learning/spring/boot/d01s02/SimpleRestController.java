88dbdb683a887bd0d44101ea9f67e4cb35784e4a
Bogdan Solga <bogdan.solga@gmail.com>
2019-04-25 14:04:43 +0300

    [improve] Improved a few module and package names

--- d01/d01s02-configuration-files-profiles-spring-web/d01s02e03-simple-rest-demo/src/main/java/com/packt/learning/spring/boot/d01s02/SimpleRestController.java
+++ d01/d01s02-configuration-files-profiles-spring-web/d01s02e03-simple-rest-demo/src/main/java/com/packt/learning/spring/boot/d01s02/SimpleRestController.java
@@ -1,4 +1,4 @@
-package com.packt.learning.spring.boot.d02s02;
+package com.packt.learning.spring.boot.d01s02;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
