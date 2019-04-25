228ee29dda4d9699c4fd45f555a06ff51ea470e0
Bogdan Solga <bogdan.solga@gmail.com>
2019-04-25 13:56:24 +0300

    [improve] Improved a few code samples, added a few YAML configuration files

--- d01/d01s02-configuration-files-profiles-spring-web/d02s02e02-bean-profiles/src/main/java/com/packt/learning/spring/boot/d02s02/BeanCommandLineProfilesDemo.java
+++ d01/d01s02-configuration-files-profiles-spring-web/d02s02e02-bean-profiles/src/main/java/com/packt/learning/spring/boot/d02s02/BeanCommandLineProfilesDemo.java
@@ -1,9 +1,9 @@
 package com.packt.learning.spring.boot.d02s02;
 
-import com.packt.learning.spring.boot.d02s02.config.BeanProfilesConfig;
-import com.packt.learning.spring.boot.d02s02.config.ProfileEnabledConfig;
 import com.packt.learning.spring.boot.d02s02.service.ProductService;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * A simple demo of a Spring project to demo the usage of {@link org.springframework.context.annotation.Bean}
@@ -11,12 +11,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  *
  * @author bogdan.solga
  */
+@SpringBootApplication
 public class BeanCommandLineProfilesDemo {
 
     public static void main(String[] args) {
         // the profiles specifying parameter ('-Dspring.profiles.active') is set as a command-line argument
-        final AnnotationConfigApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(BeanProfilesConfig.class, ProfileEnabledConfig.class);
+
+        final ConfigurableApplicationContext applicationContext = SpringApplication.run(BeanProgrammaticProfilesDemo.class);
 
         final ProductService productService = applicationContext.getBean(ProductService.class);
         productService.displayProducts();
--- d01/d01s02-configuration-files-profiles-spring-web/d02s02e02-bean-profiles/src/main/java/com/packt/learning/spring/boot/d02s02/BeanProgrammaticProfilesDemo.java
+++ d01/d01s02-configuration-files-profiles-spring-web/d02s02e02-bean-profiles/src/main/java/com/packt/learning/spring/boot/d02s02/BeanProgrammaticProfilesDemo.java
@@ -1,10 +1,9 @@
 package com.packt.learning.spring.boot.d02s02;
 
-import com.packt.learning.spring.boot.d02s02.config.BeanProfilesConfig;
-import com.packt.learning.spring.boot.d02s02.config.ProfileEnabledConfig;
 import com.packt.learning.spring.boot.d02s02.service.ProductService;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * A simple demo of a Spring project to demo the usage of {@link org.springframework.context.annotation.Bean}s
@@ -12,19 +11,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  *
  * @author bogdan.solga
  */
-@SuppressWarnings("unused")
+@SpringBootApplication
 public class BeanProgrammaticProfilesDemo {
 
     private static final String PROFILE_ACTIVATION_PROPERTY = "spring.profiles.active";
 
-    private static final String DEFAULT_PROFILE_SETTING_PROPERTY = "spring.profiles.default";
-
     // usually passed as a '-Dspring.profiles.active=<profiles>' run argument
     public static void main(String[] args) {
-        System.setProperty(PROFILE_ACTIVATION_PROPERTY, RunProfiles.DEV + "," + RunProfiles.TOMCAT);
+        System.setProperty(PROFILE_ACTIVATION_PROPERTY, RunProfiles.DEV);
 
-        final ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(BeanProfilesConfig.class, ProfileEnabledConfig.class);
+        final ConfigurableApplicationContext applicationContext = SpringApplication.run(BeanProgrammaticProfilesDemo.class);
 
         final ProductService productService = applicationContext.getBean(ProductService.class);
         productService.displayProducts();
