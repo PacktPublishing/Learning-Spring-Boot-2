ea35b0027f7a3c0c9ebebc22028646d83e2dc340
Bogdan Solga <bogdan.solga@gmail.com>
2019-04-25 19:14:20 +0300

    [improve] Improved the annotations usage, call the proper method

--- d01/d01s04-database-access/d01s04e01-using-an-embedded-database/src/main/java/com/packt/learning/spring/boot/d01s04/controller/ProductController.java
+++ d01/d01s04-database-access/d01s04e01-using-an-embedded-database/src/main/java/com/packt/learning/spring/boot/d01s04/controller/ProductController.java
@@ -6,16 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.async.DeferredResult;
-import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
 
 /**
  * A Spring {@link RestController} used to showcase the modeling of a REST controller for CRUD operations
@@ -35,46 +35,23 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @RequestMapping(
-            method = RequestMethod.POST,
-            path = ""
-    )
+    @PostMapping
     public ResponseEntity<?> create(@RequestBody Product product) {
         productService.create(product);
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
-    @RequestMapping(
-            method = RequestMethod.GET,
-            path = "/{id}"
-    )
+    @GetMapping("/{id}")
     public Product getProduct(@PathVariable final int id) {
         return productService.get(id);
     }
 
-    @RequestMapping(
-            method = RequestMethod.GET,
-            path = "/async/{id}"
-    )
-    public DeferredResult<Product> getProductAsync(@PathVariable final int id) {
-        final CompletableFuture<Product> futureProduct = CompletableFuture.supplyAsync(() -> productService.get(id));
-
-        final DeferredResult<Product> deferredResult = new DeferredResult<>();
-        deferredResult.setResult(futureProduct.join());
-
-        return deferredResult;
+    @GetMapping
+    public Iterable<Product> getAll() {
+        return productService.getAll();
     }
 
-    @RequestMapping(
-            method = RequestMethod.GET,
-            path = "/stream"
-    )
-    public StreamingResponseBody getAll() {
-        return null;//productService.findAllByName();
-    }
-
-    @RequestMapping(
-            method = RequestMethod.GET,
+    @GetMapping(
             path = {
                     "/{pageNumber}/{resultsNumber}/{sortMode}",
                     "/{pageNumber}/{resultsNumber}"
@@ -86,19 +63,13 @@ public class ProductController {
         return productService.getPage(pageNumber, resultsNumber, sortMode.orElse("asc"));
     }
 
-    @RequestMapping(
-            method = RequestMethod.PUT,
-            path = "/{id}"
-    )
+    @PutMapping("/{id}")
     public ResponseEntity<?> update(@PathVariable final int id, @RequestBody Product product) {
         productService.update(id, product);
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
-    @RequestMapping(
-            method = RequestMethod.DELETE,
-            path = "/{id}"
-    )
+    @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable final int id) {
         productService.delete(id);
         return ResponseEntity.ok(HttpStatus.OK);
