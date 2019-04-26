a7300e80a6760718f575376d78e6bc0f89bec88c
Bogdan Solga <bogdan.solga@gmail.com>
2019-04-26 13:43:26 +0300

    [dev] Added a few simple REST examples

--- d01/d01s02-configuration-files-profiles-spring-web/d01s02e03-simple-rest-demo/src/main/java/com/packt/learning/spring/boot/d01s02/SimpleRestController.java
+++ d01/d01s02-configuration-files-profiles-spring-web/d01s02e03-simple-rest-demo/src/main/java/com/packt/learning/spring/boot/d01s02/SimpleRestController.java
@@ -1,6 +1,10 @@
 package com.packt.learning.spring.boot.d01s02;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalTime;
@@ -14,9 +18,20 @@ import java.time.LocalTime;
 public class SimpleRestController {
 
     @RequestMapping(
+            method = RequestMethod.GET,
             path = "/hello"
     )
     public String hello() {
         return "Hello, Spring Boot! The current time is " + LocalTime.now();
+    }
+
+    @GetMapping("/requestParams")
+    public String requestParams(@RequestParam String color, @RequestParam String weight) {
+        return "The color is " + color + ", the weight is " + weight;
+    }
+
+    @GetMapping("/{first}/{second}")
+    public String pathVariables(@PathVariable String first, @PathVariable String second) {
+        return "The first is " + first + ", the second is " + second;
     }
 }
